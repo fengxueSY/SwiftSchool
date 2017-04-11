f0a7f36d4c5ffa1510c459d7196fde681f8c9c45
666gps <yang@yang.local>
2017-04-11 15:17:51 +0800

    Initial Commit

---
+++ SwiftSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSchool
+//
+//  Created by 666gps on 2017/4/11.
+//  Copyright © 2017年 666gps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
